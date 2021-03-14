d61249c6952a5674de7131fa8cf75f60968b0e13
july <zhangyue@coding.net>
2021-03-14 11:30:27 +0800

    test pre-commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var App = function () {
+    var title = 'A beautiful page!';
+    var desc = 'Beautiful';
+    return (<h2>{title}</h2>);
+};
+exports["default"] = App;
--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 const App = () => {
 	const title: string = 'A beautiful page!';
 	const desc = 'Beautiful';  				
-
+	
 	return ( 
 		<h2>{title}</h2>  
 	)
